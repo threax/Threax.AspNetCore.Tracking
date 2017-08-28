5cb94980f57aaf7fce9f6d2e98a9f0a84f3a67c3
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-28 08:47:54 -0400

    Fixed namespace for di.

--- TrackingExtensions.cs
+++ TrackingExtensions.cs
@@ -3,12 +3,12 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Threax.AspNetCore.Tracking;
 
-namespace Threax.AspNetCore.Tracking
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class TrackingExtensions
     {
-
         public static IServiceCollection AddTimeTracking(this IServiceCollection services)
         {
             services.TryAddSingleton<ITimestamper, Timestamper>();
